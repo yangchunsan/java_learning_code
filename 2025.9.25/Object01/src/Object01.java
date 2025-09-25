ee180b3fd01506dcdc27c94d7afbd12c280b5f0a
chengmaomao <403056885@qq.com>
2025-09-25 22:47:29 +0800

    2025/9.25 nothing

---
+++ 2025.9.25/Object01/src/Object01.java
@@ -0,0 +1,24 @@
+public class Object01 {
+    public static void main(String[] args) {
+
+        //使用oop面向对象
+        //实例化一只猫（创建一只猫）
+        Cat cat1 = new Cat();
+        //new Cat(); 创建一只猫
+        //Cat cat1 = new Cat(); 把创建的猫赋值给cat1
+        //初始化
+        cat1.name = "小白";
+        cat1.age = 8;
+        cat1.color = "black";
+    }
+}
+
+
+//定义一个猫类，自定义的数据类型
+class Cat {
+    //成员变量
+    String name;
+    int age;
+    String color;
+
+}
